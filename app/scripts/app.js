fd8e12cb925285fcb6a26690b3a53efe59c6356d
Benjamin Downs <benjamin.downs@gmail.com>
2016-01-21 21:09:56 -0600

    nearly finished with assignment 2

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,4 @@
-angular.module('confusionApp', [])
+var app = angular.module('confusionApp', [])
 
 .controller('MenuController', ['$scope', function($scope) {
 
@@ -88,24 +88,92 @@ angular.module('confusionApp', [])
 }])
 
 .controller('FeedbackController', ['$scope', function($scope) {
-  $scope.sendFeedback = function() {
-    console.log($scope.feedback);
-    if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
-      $scope.invalidChannelSelection = true;
-      console.log('incorrect');
-    } else {
-      $scope.invalidChannelSelection = false;
-      $scope.feedback = {
-        mychannel: "",
-        firstName: "",
-        lastName: "",
-        agree: false,
-        email: ""
-      };
-      $scope.feedback.mychannel = "";
+    $scope.sendFeedback = function() {
+      // console.log($scope.feedback);
+      if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+        $scope.invalidChannelSelection = true;
+        // console.log('incorrect');
+      } else {
+        $scope.invalidChannelSelection = false;
+        $scope.feedback = {
+          mychannel: "",
+          firstName: "",
+          lastName: "",
+          agree: false,
+          email: ""
+        };
 
-      $scope.feedbackForm.$setPristine();
-      console.log($scope.feedback);
-    }
+        $scope.feedback.mychannel = "";
+        $scope.feedbackForm.$setPristine();
+        console.log($scope.feedback);
+
+      }
+    };
+  }])
+  .controller('dishDetailController', function() {
+
+    var dish = {
+      name: 'Uthapizza',
+      image: 'images/uthapizza.png',
+      category: 'mains',
+      label: 'Hot',
+      price: '4.99',
+      description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+      comments: [{
+          rating: 5,
+          comment: "Imagine all the eatables, living in conFusion!",
+          author: "John Lemon",
+          date: "2012-10-16T17:57:28.556094Z"
+        }, {
+          rating: 4,
+          comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+          author: "Paul McVites",
+          date: "2014-09-05T17:57:28.556094Z"
+        }, {
+          rating: 3,
+          comment: "Eat it, just eat it!",
+          author: "Michael Jaikishan",
+          date: "2015-02-13T17:57:28.556094Z"
+        }, {
+          rating: 4,
+          comment: "Ultimate, Reaching for the stars!",
+          author: "Ringo Starry",
+          date: "2013-12-02T17:57:28.556094Z"
+        }, {
+          rating: 2,
+          comment: "It's your birthday, we're gonna party!",
+          author: "25 Cent",
+          date: "2011-12-02T17:57:28.556094Z"
+        }
+
+      ]
+    };
+
+    this.dish = dish;
+
+  })
+
+.controller("DishCommentController", ["$scope", function($scope) {
+  
+  $scope.comment = {
+    name: "",
+    rating: 5,
+    comment: "",
+    date: ""
   };
+
+  $scope.submitComment = function() {
+    var newComment = {
+      rating: $scope.comment.rating,
+      author: $scope.comment.name,
+      comment: $scope.comment.comment,
+      date: new Date().toISOString()
+    }
+    dishDetailController.$scope.dish.comments.push(newComment)
+    console.log(newComment)
+      // enter into comments
+      // clear the form ($scope.dishComment.$setPristine();)
+  };
+
+
 }]);
